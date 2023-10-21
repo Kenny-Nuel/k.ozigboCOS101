a58b53edacaa9f3cb6ac88f5a60e00cf6b54b0a4
k.ozigboCOS101 <kenny.ozigbo@gmail.com>
2023-10-21 21:45:17 +0100

    Correction of project one

--- week-2/project-1.rs
+++ week-2/project-1.rs
@@ -4,10 +4,11 @@ fn main() {
     let n:f64 = 5.0;
     let base:f64 = 1.0 + (r / 100.0);
     let exponent:f64 = base.powf(n);
+    
 
     // simple interest 
-    let a = p * exponent;
-    let amount = a.round();
+    let final_value:f64 = p * exponent;
+    let amount = final_value.round();
     println!("Amount is ₦{}", amount);
     let ci = amount - p;
     println!("Compound Interest is ₦{}", ci);
